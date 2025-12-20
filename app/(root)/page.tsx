5e6bb0d7c691b40f287dd75ea9bffe01fab927a7
NgTuanLoc <tuanloc2352000@gmail.com>
2025-12-20 10:37:08 +0700

    setup layout, sign in, sign up page

---
+++ app/(root)/page.tsx
@@ -0,0 +1,8 @@
+const Home = () => {
+    return (
+        <>
+            <h1 className="h1-bold">Welcome to the world of NextJS</h1>
+        </>
+    )
+}
+export default Home
---
+++ context/Theme.tsx
@@ -0,0 +1,9 @@
+﻿"use client"
+import {ThemeProviderProps, ThemeProvider as NextThemesProvider} from "next-themes";
+
+const Theme = ({children,...props}: ThemeProviderProps ) => {
+    return (
+        <NextThemesProvider {...props}>{children}</NextThemesProvider>
+    )
+}
+export default Theme
