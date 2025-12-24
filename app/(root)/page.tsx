082ed9f5d2ca0d1dcca1fd55356869a999875546
NgTuanLoc <tuanloc2352000@gmail.com.com>
2025-12-24 11:13:49 +0700

    Setup homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,26 @@
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+import Link from "next/link";
+
 const Home = async () => {
-  return <h1 className="h1-bold">Welcome to the world of NextJS</h1>;
+  return (
+    <>
+      {" "}
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Button className="primary-gradient text-light-900 min-h-11.5 px-4 py-3">
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">LocalSearch</section>
+      Home Filter
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question Card 1</p>
+        <p>Question Card 2</p>
+        <p>Question Card 3</p>
+        <p>Question Card 4</p>
+      </div>
+    </>
+  );
 };
 export default Home;
